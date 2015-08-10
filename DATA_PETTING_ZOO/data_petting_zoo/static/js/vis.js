e2584ab24b0aeb37f06bf87c2683740a6ff7bb5a
tlake <tanner.lake@gmail.com>
2015-08-10 15:56:49 -0700

    removed commented testing cruft

--- DATA_PETTING_ZOO/data_petting_zoo/static/js/vis.js
+++ DATA_PETTING_ZOO/data_petting_zoo/static/js/vis.js
@@ -4,7 +4,7 @@ d3.json("/static/js/project_shapefiles/uk.json", function(error, uk) {
     var width = 960;
     var height = 1160;
 
-    var svg = d3.select(".geo-map").append("svg")
+    var svg = d3.select(".gb-map").append("svg")
         .attr("width", width)
         .attr("height", height);
 
@@ -27,108 +27,3 @@ d3.json("/static/js/project_shapefiles/uk.json", function(error, uk) {
         .attr("class", function(d) { return "subunit " + d.id; })
         .attr("d", path);
 });
-
-
-
-
-
-// var dataset = [];
-
-// var projection = d3.geo.mercator()
-//                    .center([0, 5])
-//                    .scale(900)
-//                    .rotate([-180, 0]);
-
-// var svg = d3.select('.geo-map').append('svg')
-//             .attr('width', width)
-//             .attr('height', height);
-
-// var path = d3.geo.path()
-//                  .projection(projection);
-
-// var g = svg.append('g');
-
-// d3.json('world-110m2.json', function(error, topology) {
-//     g.selectAll('path')
-//         .data(topojson.object(topology, topology.objects.countries)
-//             .geometries)
-//         .enter()
-//         .append('path')
-//         .attr('d', path)
-// });
-
-// var zoom = d3.behavior.zoom()
-//              .on('zoom', function(){
-//                 g.attr('transform', 'translate(' +
-//                     d3.event.translate.join(',') + ')scale(' + d3.event.scale + ')');
-//                 g.selectAll('path')
-//                 .attr('d', path.projection(projection));
-//                 });
-
-// svg.call(zoom);
-
-// d3.csv(
-//     "modified_gb.csv",
-//     function(error, data) {
-//         if (error) {
-//             console.log(error);
-//         }
-
-//         else {
-//             dataset = data;
-
-//             var w = 1000;
-//             var h = 500;
-
-//             padding = 50;
-
-//             var xScale = d3.scale.linear()
-//                            .range([10 * padding, w - padding])
-//                            .domain(d3.extent(dataset, function(d) { return d.lng; }));
-//                            // .domain([
-//                            //      d3.min(dataset, function(d) { return d['lng']; }),
-//                            //      d3.max(dataset, function(d) { return d['lng']; })
-//                            //  ]);
-
-//             var yScale = d3.scale.linear()
-//                            .range([h - padding, padding])
-//                            .domain([
-//                                 d3.max(dataset, function(d) { return d.lat; }),
-//                                 d3.min(dataset, function(d) { return d.lat; })
-//                             ]);
-
-//             var xAxis = d3.svg.axis()
-//                               .scale(xScale)
-//                               .orient('bottom')
-//                               .ticks(5);
-
-//             var yAxis = d3.svg.axis()
-//                               .scale(yScale)
-//                               .orient('left')
-//                               .ticks(5);
-
-//             var svg = d3.select('.latlong-scatter')
-//                         .append('svg')
-//                         .attr('width', w)
-//                         .attr('height', h);
-
-//             svg.selectAll('circle')
-//                .data(dataset)
-//                .enter()
-//                .append('circle')
-//                .attr('cx', function(d) { return xScale(d.lng); } )
-//                .attr('cy', function(d) { return yScale(d.lat); } )
-//                .attr('r', 2);
-
-//             svg.append('g')
-//                 .attr('class', 'axis x-axis')
-//                 .attr('transform', 'translate(0,' + (h - padding) + ')')
-//                 .call(xAxis);
-
-//             svg.append('g')
-//                 .attr('class', 'axis y-axis')
-//                 .attr('transform', 'translate(' + padding + ',0)')
-//                 .call(yAxis);
-//         }
-//     }
-// );
