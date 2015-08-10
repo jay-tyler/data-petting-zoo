9a298bd2aec4c7cf54ae2aa5eee558222187df5c
tlake <tanner.lake@gmail.com>
2015-08-10 15:58:09 -0700

    updated home route

--- DATA_PETTING_ZOO/data_petting_zoo/views.py
+++ DATA_PETTING_ZOO/data_petting_zoo/views.py
@@ -2,6 +2,6 @@ from pyramid.view import view_config
 
 
 @view_config(route_name='home',
-             renderer='templates/index.jinja2')
+             renderer='templates/home.jinja2')
 def home_view(request):
     return {}
