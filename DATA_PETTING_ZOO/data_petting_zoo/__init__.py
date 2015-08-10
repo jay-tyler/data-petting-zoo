c871559a19df44bf81f1a142463247aa0b173eb7
tlake <tanner.lake@gmail.com>
2015-08-10 14:55:23 -0700

    jinja2 renderer declaration

--- DATA_PETTING_ZOO/data_petting_zoo/__init__.py
+++ DATA_PETTING_ZOO/data_petting_zoo/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from .models import (
     DBSession,
     Base,
-    )
+)
 
 
 def main(global_config, **settings):
@@ -13,8 +13,12 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+
+    config.include('pyramid_jinja2')
+    config.include('pyramid_tm')
+
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
