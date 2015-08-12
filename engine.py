cb2dd580787b07e2383bf327e4208362780cf4b8
Jason Tyler <jmtyler@gmail.com>
2015-08-12 09:29:45 -0700

    merged nuts3 into engine

--- engine.py
+++ engine.py
@@ -1,9 +1,13 @@
-import pandas as pd
-import numpy as np
-from pandas import Series, DataFrame
 from re import match
 from random import choice
 from string import capwords, strip
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+import fiona
+from shapely.geometry import Point, asShape
+from shapely.prepared import prep
+from shapely import speedups
 from rules import name_rules, geofeat_rules
 
 # Warn if a value is being assigned to a copy
@@ -172,6 +176,42 @@ def placename_query(df, placestring):
 
     return df[query]
     ##TODOTODOTODO
+
+
+def append_nuts3_region(dfin, shapefile_path):
+    """ Take a pandas dataframe with lat and long columns and a shapefile.
+    Convert coordinates to Shapely points to do a point-in-polygon check
+    for each point. Append name of nuts3 region corresponding to point-in-polygon
+    to the dataframe.
+    This is extremely slow. Needs to be optimized with Shapely boundary box.
+    """
+    df = dfin.copy()
+    df['nuts3'] = np.nan
+    fc = fiona.open(shapefile_path)
+    speedups.enable()
+    for feature in fc:
+        prepared_shape = prep(asShape(feature['geometry']))
+        for index, row in df.iterrows():
+            point = Point(row.loc['long'], row.loc['lat'])
+            if prepared_shape.contains(point):
+                df.loc[index, 'nuts3name'] = feature['properties']['NUTS315NM']
+                df.loc[index, 'nuts3id'] = feature['properties']['NUTS315CD']
+    return df
+
+
+def append_2013_gva(dfin, csv_file_path):
+    df = dfin.copy()
+    gva = pd.read_csv(csv_file_path)
+    gvasub = DataFrame(columns=['nuts3id', 'gva2013'])
+    gvasub['nuts3id'], gvasub['gva2013'] = gva['nutsid'], gva['2013']
+    df_gva = pd.merge(
+        left=df,
+        right=gvasub.dropna(),
+        how='left',
+        left_on='nuts3id',
+        right_on='nuts3id')
+    return df_gva
+
 
 if __name__ == "__main__":
     gbf = 'data/pristine/GB.txt'
