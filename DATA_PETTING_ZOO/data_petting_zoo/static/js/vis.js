6e73d59394e894f9f67de13daf1ae6799e82d21c
tlake <tanner.lake@gmail.com>
2015-08-12 09:59:20 -0700

    refactored global vars; working on histogram

--- DATA_PETTING_ZOO/data_petting_zoo/static/js/vis.js
+++ DATA_PETTING_ZOO/data_petting_zoo/static/js/vis.js
@@ -1,7 +1,6 @@
-var mapObject = {
-    'dataset': {},
-    'width': 760,
-    'height': 1060,
+var pZoo = {
+    'mapObj': {},
+    'popObj': {},
 };
 
 
@@ -11,7 +10,7 @@ var loadData = function(callback) {
         if (error) {
             return console.error(error);
         } else {
-            mapObject.dataset = uk;
+            pZoo.mapObj.dat = uk;
             makeDefs();
         }
     });
@@ -20,29 +19,33 @@ var loadData = function(callback) {
 
 var makeDefs = function(callback) {
 
-    mapObject.subunits = topojson.feature(
-        mapObject.dataset, mapObject.dataset.objects.subunits
+    pZoo.mapObj.width = 760;
+
+    pZoo.mapObj.height = 1060;
+
+    pZoo.mapObj.subunits = topojson.feature(
+        pZoo.mapObj.dat, pZoo.mapObj.dat.objects.subunits
     );
 
-    mapObject.projection = d3.geo.albers()
+    pZoo.mapObj.projection = d3.geo.albers()
         .center([2, 55.4])
         .rotate([4.4, 0])
         .parallels([50, 60])
         .scale(5000)
-        .translate([mapObject.width / 2, mapObject.height / 2]);
+        .translate([pZoo.mapObj.width / 2, pZoo.mapObj.height / 2]);
 
-    mapObject.path = d3.geo.path().projection(mapObject.projection);
+    pZoo.mapObj.path = d3.geo.path().projection(pZoo.mapObj.projection);
 
-    createNewSVG();
+    createNewMapSVG();
 };
 
 
-var createNewSVG = function(callback) {
+var createNewMapSVG = function(callback) {
 
     // create the SVG element that will hold the map
     var svg = d3.select(".gb-map-content").append("svg")
-        .attr("width", mapObject.width)
-        .attr("height", mapObject.height);
+        .attr("width", pZoo.mapObj.width)
+        .attr("height", pZoo.mapObj.height);
 
     drawMap();
 };
@@ -53,24 +56,24 @@ var drawMap = function() {
     d3.select("body").select('svg').selectAll('.subunit')
         .data(
             topojson
-                .feature(mapObject.dataset, mapObject.dataset.objects.subunits)
+                .feature(pZoo.mapObjd.dat, pZoo.mapObj.dat.objects.subunits)
                 .features
         )
         .enter()
         .append("path")
         .attr("class", function(d) { return "subunit " + d.id; })
-        .attr("d", mapObject.path);
+        .attr("d", pZoo.mapObj.path);
 };
 
 
 var drawLabels = function() {
 
-    if ( $.isEmptyObject(mapObject.dataset) === false ) {
+    if ( $.isEmptyObject(pZoo.mapObj.dat) === false ) {
 
-        d3.select('body').select('svg').selectAll(".subunit-label")
+        d3.select('gb-map-content').select('svg').selectAll(".subunit-label")
             .data(
                 topojson
-                .feature(mapObject.dataset, mapObject.dataset.objects.subunits)
+                .feature(pZoo.mapObj.dat, pZoo.mapObj.dat.objects.subunits)
                 .features
             )
             .enter()
@@ -78,7 +81,7 @@ var drawLabels = function() {
             .attr("class", "country-label"
             )
             .attr("transform", function(d) {
-                return "translate(" + mapObject.path.centroid(d) + ")";
+                return "translate(" + pZoo.mapObj.path.centroid(d) + ")";
             })
             .attr("dy", ".35em")
             .text(function(d) { return d.properties.name; });
@@ -88,10 +91,145 @@ var drawLabels = function() {
 
 var clearLabels = function() {
 
-    if ( $.isEmptyObject(mapObject.dataset) === false ) {
+    if ( $.isEmptyObject(pZoo.mapObj.dat) === false ) {
 
         d3.selectAll(".country-label").remove();
     }
+};
+
+
+var loadPopData = function() {
+
+    d3.json("/static/js/histogram/aberP.json", function(error, aberP) {
+        if (error) {
+            return console.error(error);
+        } else {
+            pZoo.popObj.dat = aberP;
+            createNewPopSVG();
+        }
+    });
+};
+
+
+var createNewHistoSVG = function() {
+    // var svg = d3.select(".histo-content").append("svg")
+    //     .attr("width", pZoo.width)
+    //     .attr("height", 500);
+
+    pZoo.popObj.margin = {top: 10, right: 30, bottom: 30, left: 30};
+    pZoo.popObj.width = 760 - pZoo.popObj.margin.left - pZoo.popObj.margin.right;
+    pZoo.popObj.height = 500 - pZoo.popObj.margin.top - pZoo.popObj.margin.bottom;
+
+    var svg = d3.select(".histo-content").append("svg")
+        .attr("width", pZoo.popObj.width + pZoo.popObj.margin.left + pZoo.popObj.margin.right)
+        .attr("height", pZoo.popObj.height + pZoo.popObj.margin.top + pZoo.popObj.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + pZoo.popObj.margin.left + "," + pZoo.popObj.margin.top + ")");
+
+    testHisto();
+};
+
+
+var drawHisto = function() {
+
+    var thresholds = [0, 1, 1000, 2000, 5000, 10000, 100000, 200000];
+
+    var data = d3.layout.histogram()
+        .bins(thresholds)
+        .value( pZoo.popObj.dat, function(d) { return d.pop; });
+
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(pZoo.popObj.dat, function(d) {
+            return d.pop;
+        })])
+        .range([500, 0]);
+
+    var xAxis = d3.svg.axis()
+        .ticks(thresholds);
+
+    var bar = d3.select('histo-content').select('svg').selectAll('.bar')
+        .data(data)
+        .enter()
+        .append('g')
+        .attr('class', 'bar');
+
+    bar.append('rect')
+        .attr('x', 1)
+        .attr('width', 10)
+        .attr('height', function(d) { return 500 - yScale(d.pop); });
+
+    bar.append('text')
+        .attr('dy', '0.75em')
+        .attr('y', 6)
+        .attr('x', 2)
+        .attr('text-anchor','middle')
+        .text(function(d) { return d.pop; });
+
+    d3.select('histo-content').select('svg')
+        .append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0' + 500 + ')')
+        // .call('xAxis')
+        ;
+};
+
+
+var testHisto = function() {
+    // Generate a Bates distribution of 10 random variables.
+    var values = d3.range(1000).map(d3.random.bates(10));
+
+    // A formatter for counts.
+    var formatCount = d3.format(",.0f");
+
+    var x = d3.scale.linear()
+        .domain([0, 1])
+        .range([0, pZoo.popObj.width]);
+
+    // Generate a histogram using twenty uniformly-spaced bins.
+    var data = d3.layout.histogram()
+        .bins(x.ticks(20))
+        (values);
+
+    var y = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) { return d.y; })])
+        .range([pZoo.popObj.height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var bar = d3.select('.histo-content').select('svg').selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+    bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(data[0].dx) - 1)
+        .attr("height", function(d) { return pZoo.popObj.height - y(d.y); });
+
+    bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", x(data[0].dx) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.y); });
+
+    d3.select('.histo-content').select('svg').append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + pZoo.popObj.height + ")")
+        .call(xAxis);
+};
+
+
+var createSliders = function() {
+
+    d3.select('.sidebar-content')
+        .append('slider')
+        .slider()
+        .orientation('vertical');
 };
 
 
@@ -102,4 +240,6 @@ $( document ).ready( function() {
     $( '#btn-clear-labels' ).click( function(event) { clearLabels(); });
 
     loadData();
+
+    loadHistoData();
 });
