d7d7e8177378fd6b91c272b0b613237d1aa0dbbe
Saki Fu <sakifu.86@gmail.com>
2015-08-13 13:30:37 -0700

    edited test for regex

--- test_regex.py
+++ test_regex.py
@@ -9,27 +9,19 @@ def test_aberP():
     strings = ['rat', 'mouse', 'Aberystwyth', 'Aberdyfi', 'Aberdeen', 'Abergavenny', 'Aberuthven']
     for string in strings:
         for regex in name_rules['aberP']:
-            found.append(match(regex, string).group())
-        return found
-    print found
-    assert found == ['rat']
-    # assert found == ['Aberystwyth', 'Aberdyfi', 'Aberdeen', 'Abergavenny', 'Aberuthven']
+            m = match(regex, string)
+            if m is not None:
+                found.append(m.group())
+    assert found == strings[2:]
 
 
 def test_not_aberP():
     # import ipdb; ipdb.set_trace()
-    found = []
     strings = ['rat', 'mouse']
     for string in strings:
         for regex in name_rules['aberP']:
-            if match(regex, string) is not None:
-                m = match(regex, string)
-                found.append(match(regex, string).group())
-                assert m.group() == string
-            else:
-                return 'No match'
-        return found
-    assert found == []
+            m = match(regex, string)
+            assert m is None
 
 
 def test_bergQberryS():
@@ -37,26 +29,23 @@ def test_bergQberryS():
     found = []
     strings = ['cheese', 'echidna', 'Blencathra', 'Blencogo', 'Blaenau Ffestiniog', 'Blantyre']
     for string in strings:
-        for regex in name_rules['aberP']:
-            if match(regex, string):
-                found.append(match(regex, string).group())
-            else:
-                return 'No match'
-        return found
-    assert found == ['Blencathra', 'Blencogo', 'Blaenau Ffestiniog', 'Blantyre']
+        for regex in name_rules['bergQberryS']:
+            m = match(regex, string)
+            if m is not None:
+                found.append(m.group())
+    assert found == strings[2:]
 
-
-# # def test_not_bergQberryS():
-#     found = []
-#     strings = ['cheese', 'echidna']
-#     for string in strings:
-#         for regex in name_rules['bergQberryS']:
-#             if match(regex, string):
-#                 found.append(match(regex, string).group())
-#             else:
-#                 found is None
-#         return found
-#     assert found is None
+def test_not_bergQberryS():
+  found = []
+  strings = ['cheese', 'echidna']
+  for string in strings:
+      for regex in name_rules['bergQberryS']:
+          if match(regex, string):
+              found.append(match(regex, string).group())
+          else:
+              found is None
+      return found
+  assert found is None
 
 
 def test_casterSchasterScesterSceterScaisterQxeterS():
@@ -64,10 +53,6 @@ def test_casterSchasterScesterSceterScaisterQxeterS():
     strings = ['Damaraland mole rat', 'Cape Dune mole rat', 'Lancaster', 'Doncaster', 'Gloucester', 'Caister', 'Manchester', 'Chichester', 'Worcester', 'Chester', 'Exeter', 'Cirencester', 'Colchester', 'Tadcaster', 'Leicester', 'Towcester']
     for string in strings:
         for regex in name_rules["casterSchasterScesterSceterScaisterQxeterS"]:
-            if match(regex, string):
-                found.append(match(regex, string).group())
-            else:
-                return 'No match'
+            found.append(match(regex, string).group())
         return found
     assert found == ['Doncaster', 'Gloucester', 'Caister', 'Manchester', 'Chichester', 'Worcester', 'Chester', 'Exeter', 'Cirencester', 'Colchester', 'Tadcaster', 'Leicester', 'Towcester']
-
