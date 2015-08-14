f296d2c7153fc5e1a9d7d8350eb59b4d6009c5ce
tlake <tanner.lake@gmail.com>
2015-08-13 20:40:01 -0700

    reorganizing layout

--- DATA_PETTING_ZOO/data_petting_zoo/static/js/vis.js
+++ DATA_PETTING_ZOO/data_petting_zoo/static/js/vis.js
@@ -25,9 +25,11 @@ var loadMapData = function() {
 // loadMapData() -> THIS -> createNewMapSVG()
 var makeDefs = function() {
 
-    pZoo.mapObj.width = 760;
+    // pZoo.mapObj.width = 760;
+    pZoo.mapObj.width = 600;
 
-    pZoo.mapObj.height = 1060;
+    // pZoo.mapObj.height = 1060;
+    pZoo.mapObj.height = 800;
 
     pZoo.mapObj.subunits = topojson.feature(
         pZoo.mapObj.dat, pZoo.mapObj.dat.objects.subunits
@@ -37,7 +39,7 @@ var makeDefs = function() {
         .center([2, 55.4])
         .rotate([4.4, 0])
         .parallels([50, 60])
-        .scale(5000)
+        .scale(4000)
         .translate([pZoo.mapObj.width / 2, pZoo.mapObj.height / 2]);
 
     pZoo.mapObj.path = d3.geo.path().projection(pZoo.mapObj.projection);
@@ -111,59 +113,12 @@ var drawLabels = function() {
 };
 
 
-// var plotCities = function() {
-//     d3.csv("/static/data/gb_noalt.csv", function(data) {
-//         d3.select(".map-content").select("svg").selectAll("circle")
-//         .data(data)
-//         .enter()
-//         .append("circle")
-//         .attr("cx", function(d) {
-//             return pZoo.mapObj.projection([d['long'], d['lat']])[0];
-//         })
-//         .attr("cy", function(d) {
-//             return pZoo.mapObj.projection([d['long'], d['lat']])[1];
-//         })
-//         .attr("r", 1)
-//         .style("fill", "grey")
-//         .style("opacity", 0.75)
-//     });
-// };
-
-
-// var plotPlace = function() {
-//     d3.select(".map-content").select("svg").selectAll("circle")
-//         .data([place])
-//         .enter()
-//         .append("circle")
-//         .attr("cx", function(d) {
-//             return pZoo.mapObj.projection([d['long'], d['lat']])[0];
-//         })
-//         .attr("cy", function(d) {
-//             return pZoo.mapObj.projection([d['long'], d['lat']])[1];
-//         })
-//         .attr("r", 5)
-//         .style("fill", "grey")
-//         .on("mouseover", function(d)
-//             div.transition()
-//                 .duration(100)
-//                 .style("opacity", .9);
-//             div.html(d['name'])
-//                 .style("left", (d3.event.pageX) + "px")
-//                 .style("top", (d3.event.pageY - 28) + "px");
-//             })
-//         .on("mouseout", function(d) {
-//             div.transition()
-//                 .duration(500)
-//                 .style("opacity", 0);
-//         });
-// };
-
 var plotFamily = function(response) {
 
     d3.select('.map-content').select("svg").selectAll('circle').remove();
 
     d3.select(".map-content").select("svg").selectAll("circle")
-        .data(response['fam_df'])
+        .data(response.fam_df)
         .enter()
         .append("circle")
         .attr("cx", function(d) {
@@ -222,8 +177,9 @@ var loadPopData = function() {
 var createNewPopSVG = function() {
 
     pZoo.popObj.margin = {top: 10, right: 30, bottom: 30, left: 30};
-    pZoo.popObj.width = 760 - pZoo.popObj.margin.left - pZoo.popObj.margin.right;
-    pZoo.popObj.height = 500 - pZoo.popObj.margin.top - pZoo.popObj.margin.bottom;
+    // pZoo.popObj.width = 760 - pZoo.popObj.margin.left - pZoo.popObj.margin.right;
+    pZoo.popObj.width = 600 - pZoo.popObj.margin.left - pZoo.popObj.margin.right;
+    pZoo.popObj.height = 400 - pZoo.popObj.margin.top - pZoo.popObj.margin.bottom;
 
     var svg = d3.select(".pop-content").append("svg")
         .attr("width", pZoo.popObj.width + pZoo.popObj.margin.left + pZoo.popObj.margin.right)
